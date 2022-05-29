89579c77dee77ebdff2a649fbc93f6b67c0cbd85
Mohit_kandhari <mohit.kandhari@niyotail.com>
2022-05-29 12:50:30 +0530

    up

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,37 +1,8 @@
 import React from "react";
-import DoctorImg from "../../assets/doctor.svg";
 import CardDesc from "../common/CardDesc";
 function Home() {
   return (
     <div className="container-fluid Home-page">
-      {/* <div className="row my-4 min-height-80">
-        <div className="col-lg-6 m-auto">
-          <h2 className="text-primary-without-hover">
-            Hello, Friend &#128519;
-          </h2>
-          <h2 className="text-primary-without-hover">
-            Welcome to{" "}
-            <span className="font-weight-bold text-primary-b">AmWell</span>
-          </h2>
-          <p className="text-primary-without-hover">
-            We are team of{" "}
-            <span className="font-weight-bold text-primary-b">
-              MBM Engineering College
-            </span>
-            , we will help you to provide a general symptom assessment test with
-            the help of our ML ChatBot Prediction. Lorem Ipsum is simply dummy
-            text of the printing and typesetting industry. Lorem Ipsum has been
-            the industry's standard dummy text ever since the 1500s.
-          </p>
-          <a href="https://wa.link/8l9pou">
-            <button className="btn-style btn btn-primary">Read More</button>
-          </a>
-        </div>
-        <div className="col-lg-6 m-auto">
-          <img className="w-100" src={DoctorImg} alt="amwell" />
-        </div>
-      </div> */}
-
       <main>
         <div className="position-relative overflow-hidden p-3 p-md-5 m-md-3 text-center bg-light">
           <div className="col-md-5 p-lg-5 mx-auto my-5">
