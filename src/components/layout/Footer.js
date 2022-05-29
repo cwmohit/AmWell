cdc26f0bbe714b28a368fd92e3729b91ffdd66cb
Mohit_kandhari <mohit.kandhari@niyotail.com>
2022-05-29 12:26:56 +0530

    minor fix

--- src/components/layout/Footer.js
+++ src/components/layout/Footer.js
@@ -6,13 +6,13 @@ function Footer() {
       <p className="mb-1">© 2021–2022 AmWell</p>
       <ul className="list-inline">
         <li className="list-inline-item">
-          <a href="#">Privacy</a>
+          <a href="/">Privacy</a>
         </li>
         <li className="list-inline-item">
-          <a href="#">Terms</a>
+          <a href="/">Terms</a>
         </li>
         <li className="list-inline-item">
-          <a href="#">Support</a>
+          <a href="/">Support</a>
         </li>
       </ul>
     </footer>
